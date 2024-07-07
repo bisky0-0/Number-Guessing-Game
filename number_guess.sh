40f484a8e3584f01b890e387b1e14d9a1d6187da
Biscuit krueger <yasmeinelkenany@gmail.com>
2024-07-07 22:20:10 +0000

    feat: adding the initial script for the project

--- number_guess.sh
+++ number_guess.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+PSQL="psql -X --username=freecodecamp --dbname=number_guess -t --no-align -c"
+RND=$((1+ RANDOM % 1000))
+
+
+
+
+  
+
+echo -e "Enter your username:\n"
+
+read USERNAME
+ NAME_EXISTENT=$($PSQL "SELECT username FROM users WHERE username='$USERNAME'")
+  if [[ -z $NAME_EXISTENT ]]
+    then
+       echo "Welcome, $USERNAME! It looks like this is your first time here."
+       INSERT_RESULT=$($PSQL "INSERT INTO users(username) values('$USERNAME')")
+
+       echo -e "\n Guess the secret number between 1 and 1000:\n"
+       GUESSING
+    else 
+       echo "Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses."
+       GUESSING
+  fi     
 
