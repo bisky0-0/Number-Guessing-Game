d111b7a8cb877bdd6e87bdeeac6ed4a8866f47e5
Biscuit krueger <yasmeinelkenany@gmail.com>
2024-07-07 22:20:45 +0000

    feat: add guessing function to the script

--- number_guess.sh
+++ number_guess.sh
@@ -5,6 +5,25 @@ RND=$((1+ RANDOM % 1000))
 
 
 
+GUESSING(){
+if [[ $1 ]]
+  then 
+  echo -e "$1\nGuess the secret number between 1 and 1000:\n"
+fi  
+read GUSSSED_NUM
+
+if [[ ! $GUSSSED_NUM =~ ^[0-9]+$ ]]
+  then 
+     GUESSING "That is not an integer, guess again:"
+elif [[ $GUSSSED_NUM -gt $RND ]]
+   then  GUESSING "It's lower than that, guess again:"     
+ elif [[ $GUSSSED_NUM -lt $RND ]]
+  then GUESSING "It's higher than that, guess again:"  
+ else 
+   echo "You guessed it in <number_of_guesses> tries. The secret number was <secret_number>. Nice job!"   
+fi    
+}     
+
 
   
 
